d046bebd4c93a8a69986ae866e60e8b210b9e952
xjahic <jahic.jakub@gmail.com>
2016-12-12 20:49:49 +0100

    A10

--- www-vulnerable/content/logout.php
+++ www-vulnerable/content/logout.php
@@ -7,9 +7,10 @@ if (isset($_GET["csrf"]) && $_GET["csrf"] == $_SESSION["token"]) {
 }
 
 
-/*$_SESSION = array();
-session_destroy();*/
+$_SESSION = array();
+session_destroy();
 
 //header("LOCATION: page.php") je v pohode avsak nikdy netreba verit pouzivatelovi a tak to radsej presmeruj priamo na stranku kde sa nachadza login ;)
-header("LOCATION: ".$_GET['go_page']);
+//A10
+header("LOCATION: index.php");
 ?>
